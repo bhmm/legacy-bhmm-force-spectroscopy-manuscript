7bc1d65c4c11ef35a62bb9eaa153aa974e75f6c2
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-20 18:27:40 +0100

    Debugging MLHMM initialization.

--- examples/synthetic-three-state-model/generate-figure.py
+++ examples/synthetic-three-state-model/generate-figure.py
@@ -6,31 +6,35 @@ Generate plots for synthetic three-state force spectroscopy model.
 
 from bhmm import testsystems
 from bhmm import BHMM
+from bhmm import plots
 
 # Create model.
 true_model = testsystems.force_spectroscopy_model()
+nstates = true_model.nstates
 
 # Generate synthetic data.
 [O, S] = true_model.generate_synthetic_observation_trajectories(ntrajectories=1, length=50000)
 
-
+# DEBUG
+print "synthetic observation trajectories:"
+print O
+print "Total state visits, min_state, max_state:"
+print testsystems.total_state_visits(nstates, S)
 
 # Initialize BHMM.
 tau = 0.001 # time interval per observation
-nstates = 3
-bhmm = BHMM(nstates, O)
+bhmm = BHMM(O, nstates)
 
 # Sample models.
 models = bhmm.sample(nsamples=10, save_hidden_state_trajectory=False)
-
-# Extract hidden state trajectories and observations.
-s_t = S[0]
-o_t = O[0]
 
 # Generate a sample saving a hidden state trajectory.
 models = bhmm.sample(nsamples=1, save_hidden_state_trajectory=True)
 
 # Plot.
-plot_state_assignments(model, s_t, o_t, time_units='s', obs_label='force / pN', tau=tau, pdf_filename='synthetic-three-state-model.pdf')
+model = models[0]
+s_t = model.hidden_state_trajectories[0]
+o_t = O[0]
+plots.plot_state_assignments(model, s_t, o_t, time_units='s', obs_label='force / pN', tau=tau, pdf_filename='synthetic-three-state-model.pdf')
 
 
