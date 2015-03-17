425d3109563b2e657b164922634cd7b9fa6ea3ef
noe <frank.noe@fu-berlin.de>
2015-03-17 10:58:56 +0100

    corrected bugs occurring with different observation trajectory lengths

---
+++ examples/discrete_1d_2min/run.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pyemma.msm.io as msmio
+import pyemma.msm.analysis as msmana
+from bhmm import MLHMM
+
+# load observations
+o = np.loadtxt('2well_traj_100K.dat', dtype=int)
+
+# multiple lags
+lags = [1,2,5,10,20,50,100,200,500,1000]
+its  = np.zeros((len(lags)))
+for (i,lag) in enumerate(lags):
+    # prepare shifted lagged data
+    observations = []
+    for shift in range(0, lag):
+        observations.append(o[shift:][::lag])
+
+    # initial HMM
+    hmm = MLHMM(observations, 2, kernel='c')
+    hmm.fit()
+    P = hmm.model.Tij
+    its[i] = msmana.timescales(P, tau=lag)[1]
+
+print 'Reference:'
+P = msmio.read_matrix('2well_P.dat', mode='sparse').toarray()
+itsref = msmana.timescales(P, tau=0.1)[1]
+print itsref
+
+print 'Resulting timescales:'
+for i in range(len(lags)):
+    print lags[i], its[i]
